5fdc6642ed8ec94c9f58965341776ccc80411ab9
Ahmet Sağırlı <ahmtsgrl@gmail.com>
2024-06-08 02:01:04 +0300

    Rev.....

--- src/components/ThemeSwitch/ThemeSwitch.tsx
+++ src/components/ThemeSwitch/ThemeSwitch.tsx
@@ -12,8 +12,6 @@ import { user } from '@spectrumlabs/analytics';
 import { FC, useCallback } from 'react';
 
 import { Theme, useApplicationSettings } from '../../context';
-import HoskyLogo from './hoski-logo.jpeg';
-import SnekLogo from './snek-logo.jpeg';
 
 export const ThemeSwitch: FC = () => {
   const [settings, setSettings] = useApplicationSettings();
@@ -30,56 +28,6 @@ export const ThemeSwitch: FC = () => {
     },
     [settings, setSettings],
   );
-
-  const DropdownOverlay = (): JSX.Element => {
-    return (
-      <Menu>
-        <Menu.Item onClick={() => handleChangeTheme('snek')}>
-          <Flex>
-            <Flex.Item marginRight={2}>
-              <img
-                style={{ borderRadius: '999px' }}
-                alt="Snek logo"
-                src={SnekLogo}
-                width={21}
-                height={21}
-              />
-            </Flex.Item>
-            <Flex.Item>Snek (light)</Flex.Item>
-          </Flex>
-        </Menu.Item>
-        <Menu.Item onClick={() => handleChangeTheme('snek-dark')}>
-          <Flex>
-            <Flex.Item marginRight={2}>
-              <img
-                style={{ borderRadius: '999px' }}
-                alt="Snek logo"
-                src={SnekLogo}
-                width={21}
-                height={21}
-              />
-            </Flex.Item>
-            <Flex.Item>Snek (dark)</Flex.Item>
-          </Flex>
-        </Menu.Item>
-        <Menu.Item
-          style={{ display: 'none' }}
-          onClick={() => handleChangeTheme('hosky')}
-        >
-          <Flex.Item marginRight={2}>
-            <img
-              style={{ borderRadius: '999px' }}
-              alt="Hosky logo"
-              src={HoskyLogo}
-              width={21}
-              height={21}
-            />
-          </Flex.Item>
-          <Flex.Item>Hosky theme</Flex.Item>
-        </Menu.Item>
-      </Menu>
-    );
-  };
 
   return (
     <Flex col>
@@ -101,13 +49,6 @@ export const ThemeSwitch: FC = () => {
             <Tabs.TabPane tab={t`System`} key="system" />
           </Tabs>
         </Flex.Item>
-        {/* <Flex.Item align="center">
-          <Dropdown overlay={<DropdownOverlay />}>
-            <Button block size="small">
-              Meme theme <DownOutlined />
-            </Button>
-          </Dropdown>
-        </Flex.Item> */}
       </Flex>
     </Flex>
   );
