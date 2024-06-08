fc50eea7f60024e5bf2dc704453836ae84d96680
Ahmet Sağırlı <ahmtsgrl@gmail.com>
2024-06-08 03:07:41 +0300

    Rev.

--- src/components/common/Layout/Header/BurgerMenu/BurgerMenu.tsx
+++ src/components/common/Layout/Header/BurgerMenu/BurgerMenu.tsx
@@ -1,10 +1,14 @@
 import {
   Button,
   Dropdown,
+  FileTextOutlined,
+  GithubOutlined,
   GlobalOutlined,
+  InfoCircleOutlined,
   LeftOutlined,
   Menu,
   Modal,
+  QuestionCircleOutlined,
   ReloadOutlined,
   RightOutlined,
 } from '@ergolabs/ui-kit';
@@ -61,6 +65,26 @@ const BurgerMenu = (): JSX.Element => {
 
   const menu = [
     {
+      title: t`About`,
+      icon: <InfoCircleOutlined />,
+      link: 'https://docs.spectrum.fi/docs/about-spectrumdex/intro',
+    },
+    {
+      title: t`How to use`,
+      icon: <QuestionCircleOutlined />,
+      link: 'https://docs.spectrum.fi/docs/user-guides/quick-start',
+    },
+    {
+      title: t`Docs`,
+      icon: <FileTextOutlined />,
+      link: 'https://docs.spectrum.fi',
+    },
+    {
+      title: 'GitHub',
+      icon: <GithubOutlined />,
+      link: 'https://github.com/spectrum-finance',
+    },
+    {
       title: t`Manual Tx Cancel`,
       icon: <ReloadOutlined />,
       onClick: () => {
@@ -93,10 +117,11 @@ const BurgerMenu = (): JSX.Element => {
         <ThemeSwitch />
       </ThemeSwitchContainer>
       {menu.map(
-        (item: { title: string; icon: JSX.Element; onClick: () => void; additional?: JSX.Element; link?: string }, index) =>
+        (item, index) =>
           item && (
             <OtherMenuItem key={index + 1} icon={item.icon}>
               <a
+                href={item.link}
                 rel="noreferrer"
                 target={item.link ? '_blank' : ''}
                 onClick={item.onClick}
