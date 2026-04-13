99cc0c664330439169e4f413e01265df4662b2be
Ahmet Sağırlı <ahmtsgrl@gmail.com>
2026-04-13 11:59:56 +0300

    Update ErgoPayModal link for unsigned transactions

--- src/network/ergo/widgets/ErgoPayModal/common/ergopayLinks.ts
+++ src/network/ergo/widgets/ErgoPayModal/common/ergopayLinks.ts
@@ -2,7 +2,7 @@ import { applicationConfig } from '../../../../../applicationConfig';
 import { TxId } from '../../../../../common/types';
 
 export const createUnsignedTxRequestLink = (txId: TxId): string =>
-  `${applicationConfig.networksSettings.ergo.ergopayUrl}/unsignedTx/${txId}`;
+  `${applicationConfig.networksSettings.ergo.ergopayUrl}/get?id=${txId}`;
 
 export const createSelectAddressesRequestLink = (requestId: string): string =>
   `${applicationConfig.networksSettings.ergo.ergopayUrl}/auth?id=${requestId}&address=#P2PK_ADDRESS#`;
