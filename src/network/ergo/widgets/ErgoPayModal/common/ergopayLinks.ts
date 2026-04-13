c791e9d6f122811b294587303dcb2d0c17f12534
Ahmet Sağırlı <ahmtsgrl@gmail.com>
2026-04-13 11:59:45 +0300

    Update ergopayLinks.ts

--- src/network/ergo/widgets/ErgoPayModal/common/ergopayLinks.ts
+++ src/network/ergo/widgets/ErgoPayModal/common/ergopayLinks.ts
@@ -5,7 +5,7 @@ export const createUnsignedTxRequestLink = (txId: TxId): string =>
   `${applicationConfig.networksSettings.ergo.ergopayUrl}/unsignedTx/${txId}`;
 
 export const createSelectAddressesRequestLink = (requestId: string): string =>
-  `${applicationConfig.networksSettings.ergo.ergopayUrl}/addresses/${requestId}/#P2PK_ADDRESS#`;
+  `${applicationConfig.networksSettings.ergo.ergopayUrl}/auth?id=${requestId}&address=#P2PK_ADDRESS#`;
 
 export const createErgoPayDeepLink = (requestLink: string): string =>
   requestLink.replace('https', 'ergopay');
